bce78cfb040e6ea1ccc4ef1d1dada8c49c34ecdc
desertfoxs <angelolivetti78@gmail.com>
2024-09-23 20:39:34 -0300

    commit con la tabla lista

--- users.php
+++ users.php
@@ -30,7 +30,7 @@ $conn->close();
     <title>listaUsuarios</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
-
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.css" />
 </head>
 
 <body>
@@ -47,31 +47,65 @@ $conn->close();
 
     <div class="container">
         <h2>Lista de Usuarios</h2>
-        <div class="user-list">
+        <table id="myTable" class="display">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>NickName</th>
+                    <th>Email</th>
+                    <th>Rol</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
 
             <?php
             if ($resultado) {
                 while ($row = $resultado->fetch_array()) {
-                    $ID = $row['id'];
                     $nombre = $row['nombre'];
                     $apellido = $row['apellido'];
                     $nickname = $row['nickname'];
                     $email = $row['email'];
                     $rol = $row['rol'];
-
                     ?>
 
-                    <div class="user-card"><?php echo $nombre . "\n" . 
-                                                $apellido?></div>
+                    <!-- aca agregar la lista-->
+
+                    <tbody>
+                        <tr>
+                            <td> <?php echo $nombre?> </td>
+                            <td> <?php echo  $apellido?> </td>
+                            <td> <?php echo $nickname?> </td>
+                            <td> <?php echo $email?> </td>
+                            <td> <?php echo $rol?> </td>
+                            <td> <a href="">eliminar</a> -- <a href="">modificar</a> </td>
+                        </tr>
+                    </tbody>
 
                     <?php
                 }
             }
             ?>
-        </div>
+
+
+        </table>
+
     </div>
-
-
 </body>
 
 </html>
+
+<script src="https://code.jquery.com/jquery-3.7.1.slim.js"
+    integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+
+<script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
+
+
+<script>
+
+    // aca agregamos el link de la tabla
+    $(document).ready(function () {
+        $('#myTable').DataTable();
+    });
+
+</script>
